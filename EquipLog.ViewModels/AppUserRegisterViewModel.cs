743247e459547e499023102a2e98026662d4c576
krasennikolicov@abv.bg <krasennikolicov2@abv.bg>
2025-04-28 20:02:50 +0300

    Add user controller, add toast

---
+++ EquipLog.ViewModels/AppUserRegisterViewModel.cs
@@ -0,0 +1,35 @@
+﻿using EquipLog.Constants;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace EquipLog.ViewModels
+{
+    public class AppUserRegisterViewModel
+    {
+       
+        [Required]
+        [MaxLength(EntityValidationsConstants.ApplicationUserNameMaxLenght)]
+        [MinLength(EntityValidationsConstants.ApplicationUserCorporateIDMinLength)]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(EntityValidationsConstants.ApplicationUserSureNameMaxLength)]
+        [MinLength(EntityValidationsConstants.ApplicationUserSureNameMinLength)]
+        public string Surname { get; set; }
+        [Required]
+        [MaxLength(EntityValidationsConstants.DepartmentMaxLength)]
+        public string Department { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [DataType(DataType.Password)]
+        [Compare("Password",ErrorMessage ="Password does not match")]
+        public string ConfirmPassword { get; set; }
+        
+        [Required]
+        [MaxLength(EntityValidationsConstants.ApplicationUserCorporateIDMaxLength)]
+        [MinLength(EntityValidationsConstants.ApplicationUserCorporateIDMinLength)]
+        public string CorporateID { get; set; }
+    }
+}
---
+++ EquipLog/Controllers/AppUserController.cs
@@ -0,0 +1,30 @@
+﻿using EquipLog.Data.SQL.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EquipLog.Controllers
+{
+    public class AppUserController : Controller
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IUserStore<ApplicationUser> _userStore;
+
+        public AppUserController()
+        {
+                
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+
+            return View();
+        }
+        //public IActionResult Register() 
+        //{
+
+        //}
+       
+    }
+}
